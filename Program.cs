2e547f2ec7b5aeb8477c4c7c925fb3084782613d
NigDino <danilaodinec0@gmail.com>
2023-03-13 15:43:07 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+
+    public class Receipt
+    {
+        int number;
+        string date;
+        float amount;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
